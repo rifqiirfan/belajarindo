8f0b8883f6a7d958eb49cee935f5941137b13ef4
RifqyAir <muhrifh@gmail.com>
2025-02-20 22:04:54 +0700

    adjust: add difficulty level in achievement

--- belajarindo/app/(authorized)/v1/achievements/_components/columns.tsx
+++ belajarindo/app/(authorized)/v1/achievements/_components/columns.tsx
@@ -9,6 +9,7 @@ import { AchievementsDataTypes } from "@/core/models/achievement.model";
 export const columns: ColumnDef<AchievementsDataTypes>[] = [
   generateSortableColumn("name"),
   generateSortableColumn("description"),
+  generateSortableColumn("difficulty_level", "Level"),
   generateSortableColumn("achieve_point", 'Achieve At'),
   generateSortableColumn("icon_url"),
   {
