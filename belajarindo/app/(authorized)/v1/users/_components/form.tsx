87ab8ab29830aef8d29b6b11dc12926de59a432a
Vijjasena <work.vijjasena@gmail.com>
2025-02-22 00:50:00 +0700

    wire master data user

--- belajarindo/app/(authorized)/v1/users/_components/form.tsx
+++ belajarindo/app/(authorized)/v1/users/_components/form.tsx
@@ -85,9 +85,6 @@ export default function FormUsers({data, type}: FormPageProps) {
                              label={'Date of Birth'}/>
             <InputBasic name={"country"} required={true} disabled={type === "detail"}/>
             <div className="flex gap-2">
-              <pre>
-                {JSON.stringify(form.formState.errors, null, 2)}
-              </pre>
               {type !== "detail" && <Button type="submit">Submit</Button>}
               <Button type="button" variant={'outline'} asChild><Link href={'/v1/users'}>Back</Link></Button>
             </div>
