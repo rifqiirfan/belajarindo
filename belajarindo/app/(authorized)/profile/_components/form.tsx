c6640f0bc7413ca07521542b2b7668bde603f34d
Vijjasena <work.vijjasena@gmail.com>
2025-02-23 13:58:50 +0700

    update profile

--- belajarindo/app/(authorized)/profile/_components/form.tsx
+++ belajarindo/app/(authorized)/profile/_components/form.tsx
@@ -78,7 +78,7 @@ export default function FormProfile({data}: Omit<FormPageProps, "type">) {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <InputBasic name={"full_name"} required={true} disabled/>
             <InputBasic name={"email"} required={true} disabled/>
-            <InputBasic name={"username"} required={true} disabled/>
+            <InputBasic name={"username"} required={true}/>
             <InputBasic inputProps={{type: "password"}} name={"password"} required={true}/>
             <InputBasic inputProps={{type: "password"}} name={"confirm_password"} required={true}/>
             <div className="flex gap-2">
